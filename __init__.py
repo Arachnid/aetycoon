316e36aa5e6792bdc5c010454aa45a04c814eb34
Nicholas Johnson <nickjohnson@dhcp-172-26-169-51.dub.corp.google.com>
2009-10-08 15:57:42 +0100

    Added PickleProperty

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import hashlib
 import logging
+import pickle
 from google.appengine.ext import db
 
 
@@ -243,6 +244,7 @@ class _TransformProperty(db.Property):
   def __set__(self, model_instance, value):
     raise db.DerivedPropertyError("Cannot assign to a TransformProperty")
 
+
 class KeyProperty(db.Property):
   """A property that stores a key, without automatically dereferencing it.
   
@@ -274,3 +276,35 @@ class KeyProperty(db.Property):
         raise TypeError("Property %s must be an instance of db.Key"
                         % (self.name,))
     return super(KeyProperty, self).validate(value)
+
+
+class PickleProperty(db.Property):
+  """A property for storing complex objects in the datastore in pickled form.
+  
+  Example usage:
+  
+  >>> class PickleModel(db.Model):
+  ...   data = PickleProperty()
+
+  >>> model = PickleModel()
+  >>> model.data = {"foo": "bar"}
+  >>> model.data
+  {'foo': 'bar'}
+  >>> model.put() # doctest: +ELLIPSIS
+  datastore_types.Key.from_path(u'PickleModel', ...)
+  
+  >>> model2 = PickleModel.all().get()
+  >>> model2.data
+  {'foo': 'bar'}
+  """
+  
+  data_type = db.Blob
+
+  def get_value_for_datastore(self, model_instance):
+    value = self.__get__(model_instance, model_instance.__class__)
+    if value is not None:
+      return db.Blob(pickle.dumps(value))
+
+  def make_value_from_datastore(self, value):
+    if value is not None:
+      return pickle.loads(str(value))
