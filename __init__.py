f9895ef0519a373531525c91a90e0d6540e009fc
Nick Johnson <arachnid@notdot.net>
2009-10-17 15:38:48 +0100

    Added SetProperty

--- __init__.py
+++ __init__.py
@@ -308,3 +308,49 @@ class PickleProperty(db.Property):
   def make_value_from_datastore(self, value):
     if value is not None:
       return pickle.loads(str(value))
+
+
+class SetProperty(db.ListProperty):
+  """A property that stores a set of things.
+
+  This is a parameterized property; the parameter must be a valid
+  non-list data type, and all items must conform to this type.
+  
+  Example usage:
+  
+  >>> class SetModel(db.Model):
+  ...   a_set = SetProperty(int)
+  
+  >>> model = SetModel()
+  >>> model.a_set = set([1, 2, 3])
+  >>> model.a_set
+  set([1, 2, 3])
+  >>> model.a_set.add(4)
+  >>> model.a_set
+  set([1, 2, 3, 4])
+  >>> model.put() # doctest: +ELLIPSIS
+  datastore_types.Key.from_path(u'SetModel', ...)
+  
+  >>> model2 = SetModel.all().get()
+  >>> model2.a_set
+  set([1L, 2L, 3L, 4L])
+  """
+
+  def validate(self, value):
+    value = db.Property.validate(self, value)
+    if value is not None:
+      if not isinstance(value, (set, frozenset)):
+        raise db.BadValueError('Property %s must be a set' % self.name)
+
+      value = self.validate_list_contents(value)
+    return value
+
+  def default_value(self):
+    return set(db.Property.default_value(self))
+
+  def get_value_for_datastore(self, model_instance):
+    return list(super(SetProperty, self).get_value_for_datastore(model_instance))
+
+  def make_value_from_datastore(self, value):
+    if value is not None:
+      return set(super(SetProperty, self).make_value_from_datastore(value))
